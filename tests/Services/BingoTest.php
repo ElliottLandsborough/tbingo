4686194fc202aef977b824df7d1ac3ce30f28223
Elliott Landsborough <elliott.landsborough@gmail.com>
2022-12-15 23:30:35 +0000

    A blank unit test

---
+++ tests/Services/BingoTest.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace ElliottLandsborough\TerminalBingo\Tests\Services;
+
+use ElliottLandsborough\TerminalBingo\Services\Bingo;
+use Exception;
+use PHPUnit\Framework\TestCase;
+
+class BingoTest extends TestCase
+{
+    protected $bingo;
+
+    // runs per test
+    public function setUp(): void
+    {
+        $this->bingo = new Bingo();
+    }
+
+    // runs after each test
+    public function tearDown(): void
+    {
+        $this->bingo = null;
+    }
+
+    public function testNothing()
+    {
+    }
+}
