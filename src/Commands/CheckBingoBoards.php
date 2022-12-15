f6fd707b7243d76baea187238695255f70c61f13
Elliott Landsborough <elliott.landsborough@gmail.com>
2022-12-15 20:02:31 +0000

    A blank command line class

---
+++ src/Commands/CheckBingoBoards.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace ElliottLandsborough\TerminalBingo\Commands;
+
+use ElliottLandsborough\TerminalBingo\Services\Bingo;
+use Symfony\Component\Console\Attribute\AsCommand;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Helper\Table;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+
+#[AsCommand(name: 'checkBoards', description: 'Check some bingo input')]
+class CheckBingoBoards extends Command
+{
+    /**
+     * Execute the command.
+     *
+     * @param InputInterface  $input
+     * @param OutputInterface $output
+     *
+     * @return int 0 if everything went fine, or an exit code.
+     */
+    protected function execute(
+        InputInterface $input,
+        OutputInterface $output
+    ): int {
+        $bingo = new Bingo();
+
+        return Command::SUCCESS;
+    }
+}
---
+++ src/Services/Bingo.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace ElliottLandsborough\TerminalBingo\Services;
+
+use Exception;
+
+class Bingo
+{
+    /*
+        private function _stdin_stream()
+        {
+            while ($line = fgets(STDIN)) {
+                yield $line;
+            }
+        }
+
+        foreach (stdin_stream() as $line) {
+            // do something with the contents coming in from STDIN
+        }
+    */
+}
