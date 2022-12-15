56ab4de3166895bd65ac06ba89c69f668cd37b50
Elliott Landsborough <elliott.landsborough@gmail.com>
2022-12-15 23:58:55 +0000

    Something is broken

--- exec.sh
+++ exec.sh
@@ -1,1 +1,21 @@
-cat input.txt | ./bin/app checkBoards
+#!/usr/bin/env bash
+
+set -o errexit
+set -o nounset
+set -o pipefail
+if [[ "${TRACE-0}" == "1" ]]; then
+    set -o xtrace
+fi
+
+if [[ "${1-}" =~ ^-*h(elp)?$ ]]; then
+    echo 'Usage: ./exec.sh'
+    exit
+fi
+
+cd "$(dirname "$0")"
+
+main() {
+    ./bin/app checkBoards < input.txt
+}
+
+main "$@"
