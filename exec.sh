3933ded3f75335cf214b516bb8108414a3f66974
Elliott Landsborough <elliott.landsborough@gmail.com>
2022-12-16 00:10:47 +0000

    Shorten script a bit

--- exec.sh
+++ exec.sh
@@ -7,15 +7,6 @@ if [[ "${TRACE-0}" == "1" ]]; then
     set -o xtrace
 fi
 
-if [[ "${1-}" =~ ^-*h(elp)?$ ]]; then
-    echo 'Usage: ./exec.sh'
-    exit
-fi
-
 cd "$(dirname "$0")"
 
-main() {
-    ./bin/app checkBoards < input.txt
-}
-
-main "$@"
+./bin/app checkBoards < input.txt
